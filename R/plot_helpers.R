b0b4d800d4dda340113912fd5199607a819540d4
Edward Wallace <ewjwallace@gmail.com>
2019-10-26 21:47:44 +0100

    scale_x_log2/log10

--- R/plot_helpers.R
+++ R/plot_helpers.R
@@ -1,6 +1,28 @@
 ## plot helper functions
 
-scale_y_log2nice <- function(name=waiver(),omag=seq(-6,6),scilabels=FALSE,...) {
+scale_x_log2nice <- function(name=waiver(),omag=seq(-10,10),scilabels=FALSE,...) {
+    # plot x axis on log2-scale with nice defaults
+    breaks2 <- 2^omag
+    if (scilabels) {
+        labels2 <- paste("2^{",omag,"}",sep="")
+    } else {
+        labels2 <- breaks2
+    }
+    scale_x_log10(name,breaks=breaks2,labels=parse(text=labels2),...)
+}
+
+scale_x_log10nice <- function(name=waiver(),omag=seq(-10,10),scilabels=FALSE,...) {
+    # plot x axis on log10-scale with nice defaults
+    breaks10 <- 10^omag
+    if (scilabels) {
+        labels10 <- paste("10^{",omag,"}",sep="")
+    } else {
+        labels10 <- breaks10
+    }
+    scale_x_log10(name,breaks=breaks10,labels=parse(text=labels10),...)
+}
+
+scale_y_log2nice <- function(name=waiver(),omag=seq(-10,10),scilabels=FALSE,...) {
     # plot y axis on log2-scale with nice defaults
     breaks2 <- 2^omag
     if (scilabels) {
@@ -11,7 +33,7 @@ scale_y_log2nice <- function(name=waiver(),omag=seq(-6,6),scilabels=FALSE,...) {
     scale_y_log10(name,breaks=breaks2,labels=parse(text=labels2),...)
 }
 
-scale_y_log10nice <- function(name=waiver(),omag=seq(-6,6),scilabels=FALSE,...) {
+scale_y_log10nice <- function(name=waiver(),omag=seq(-10,10),scilabels=FALSE,...) {
     # plot y axis on log10-scale with nice defaults
     breaks10 <- 10^omag
     if (scilabels) {
@@ -20,4 +42,26 @@ scale_y_log10nice <- function(name=waiver(),omag=seq(-6,6),scilabels=FALSE,...) 
         labels10 <- breaks10
     }
     scale_y_log10(name,breaks=breaks10,labels=parse(text=labels10),...)
+}
+
+scale_x_log2nice <- function(name=waiver(),omag=seq(-10,10),scilabels=FALSE,...) {
+    # plot  axis on log2-scale with nice defaults
+    breaks2 <- 2^omag
+    if (scilabels) {
+        labels2 <- paste("2^{",omag,"}",sep="")
+    } else {
+        labels2 <- breaks2
+    }
+    scale_x_log10(name,breaks=breaks2,labels=parse(text=labels2),...)
+}
+
+scale_x_log10nice <- function(name=waiver(),omag=seq(-10,10),scilabels=FALSE,...) {
+    # plot x axis on log10-scale with nice defaults
+    breaks10 <- 10^omag
+    if (scilabels) {
+        labels10 <- paste("10^{",omag,"}",sep="")
+    } else {
+        labels10 <- breaks10
+    }
+    scale_x_log10(name,breaks=breaks10,labels=parse(text=labels10),...)
 }
