3c76dadb7a106127498d7c4d7c6e2a4a7487604a
Edward Wallace <ewjwallace@gmail.com>
2019-10-26 22:45:42 +0100

    docs for amp_melt_curve_functions.R

--- R/amp_melt_curve_functions.R
+++ R/amp_melt_curve_functions.R
@@ -1,4 +1,30 @@
 
+
+#' Remove baseline from amplification curves (BETA)
+#' 
+#' Remove baseline from qPCR amplification curves 
+#' by subtracting median of initial cycles.
+#' 
+#' BETA function version because:
+#' 
+#' - assumes Roche Lightcycler format, 
+#' we should ideally replace "program==2" by something more generic?
+#' 
+#' - the rule-of thumb "baseline is median of initial 10 cycles" 
+#' has not been tested robustly
+#'
+#' @param plateamp data frame with plate amplification data, including variables
+#'   Well, Cycle, Fluor (fluorescence value), and Program. Assume program 2 for
+#'   cmplification curves from Roche Lightcycler format data.
+#' @param maxcycle maximum cycle value to use for baseline, before
+#'   amplification.
+#'   
+#' @return platemap with additional columns per Well: 
+#' \tabular{ll}{
+#'   Base      \tab baseline /background value  \cr
+#'   Signal    \tab normalized fluorescence signal, i.e. Fluor - Base
+#'   }
+#'
 debaseline <- function(plateamp,maxcycle=10) {
     baseline <- 
         plateamp %>%
@@ -11,6 +37,18 @@ debaseline <- function(plateamp,maxcycle=10) {
         
 }
 
+
+#' Calculate dR/dT for a melt curve of signal R vs temperature T
+#'
+#' @param TT Temperature.
+#' @param RR Signal, assumed fluorescence signal matched to TT.
+#' @param method to use, defaulting to a smoothing spline.
+#' @param ... other arguments to pass to smoothing method.
+#'
+#' @return estimated first derivative of RR with respect to TT, 
+#' numeric vector of same length as RR.
+#'
+#' @family melt_curve_functions
 getdRdT <- function(TT,RR,method=c("spline","diff"),...) {
     if (method == "diff") {
        return( -c(diff(RR)/diff(TT),NA) )
@@ -20,10 +58,21 @@ getdRdT <- function(TT,RR,method=c("spline","diff"),...) {
     }
 }
 
-demelt <- function(platemelt) {
+
+#' Calculate dR/dT for melt curves of every well in a plate
+#'
+#' @param platemelt data frame describing melt curves, including variables
+#'   Well, Temperature, Fluor (fluorescence value).
+#'
+#' @return platemelt with additional column dRdT.
+#'
+#' @family melt_curve_functions
+getdRdTall <- function(platemelt) {
     platemelt %>%
         arrange(Well,Temperature) %>%
-        group_by(Well) %>%
-        mutate(dRdT=getdRdT(Temperature,Signal)) %>%
+        # @ewallace: doesn't group by plate, only by well,
+        # so will fail strangely if used on data from multiple plates
+        group_by(Well) %>% 
+        mutate(dRdT=getdRdT(Temperature,Fluor)) %>%
         ungroup()
 }
